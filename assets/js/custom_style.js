8e5349ea745b07b18e9b33337fa1d8b02b4e634a
Ghani <ghani@eyrox.com>
2022-09-10 05:19:41 +0500

    Final changes

--- assets/js/custom_style.js
+++ assets/js/custom_style.js
@@ -1,62 +1,9 @@
-$(document).ready(function(){
-	$(window).scroll(function () {
-			if ($(this).scrollTop() > 50) {
-				$('#back-to-top').fadeIn();
-			} else {
-				$('#back-to-top').fadeOut();
-			}
-		});
-		// scroll body to 0px on click
-		$('#back-to-top').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 400);
-			return false;
-        });
-
-        $(window).scroll(function () {
-			if ($(this).scrollTop() > 200) {
-				$('header').addClass("fixed");
-			} else {
-				$('header').removeClass("fixed");
-			}
-		});  
-		
-
-        $(window).scroll(function () {
-			if ($(this).scrollTop() > 50) {
-				$('.gift-page header').addClass("fixed");
-			} else {
-				$('.gift-page header').removeClass("fixed");
-			}
-        });  
-        
-        $(".scrollTo").on('click', function(e) {
-            e.preventDefault();
-            var target = $(this).attr('href');
-            $('html, body').animate({
-              scrollTop: ($(target).offset().top - 140)
-            }, 2000);
-		 });
-		 
-		 $("a.menu-icon").click(function(){
-			$(".mobile-menu ul").show();
-		  });
-
-		  $("a.cross").click(function(){
-			$(".mobile-menu ul").hide();
-		  });
-
-		  $("a.toggle-btn").click(function(){
-			$(".covid-detail").slideToggle(500);
-			$("a.toggle-btn i").toggleClass('fa-minus');
-		  });
+$(".menu-icon").click(function(){
+    $(".menu-area").show();
 });
-$(".mobile-menu a").click(function(){
-    $(".list-menu").slideToggle();
+$(".cross").click(function(){
+    $(".menu-area").hide();
 });
-$('.list-menu > li > i').click(function(){
-    event.preventDefault();
-	$('.submenu').slideUp('slow');
-    $(this).parent().find('.submenu').slideToggle('slow');
+$(".s-menu").click(function(){
+    $(".menu-area .sub-menu").slideToggle();
 });
